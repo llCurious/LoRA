ad75f738490aa39d114e37e44f87419d017ceb56
llCurious <haoqi.whq@antgroup.com>
2023-12-04 10:53:27 +0800

    update eval script

---
+++ examples/NLG/src/utils/data_utils.py
@@ -0,0 +1,52 @@
+import os
+import datasets
+
+def get_dataset(dataset_name, type: str = "validation"):
+    if dataset_name == "wikitext":
+        data_dir = os.path.join(
+            os.path.expanduser("~"), ".cache/huggingface/datasets/wikitext"
+        )
+        dataset = datasets.load_from_disk(data_dir)[type]
+    elif dataset_name == "lambada":
+        data_dir = os.path.join(
+            os.path.expanduser("~"), 
+            ".cache/huggingface/datasets/lambada"
+        )
+        dataset = datasets.load_dataset(data_dir, split=type)
+    elif dataset_name in ["cola", "rte", "qnli", "mnli"]:
+        data_dir = os.path.join(
+            os.path.expanduser("~"), 
+            ".cache/huggingface/datasets/glue"
+        )
+        dataset = datasets.load_dataset("glue", dataset_name, cache_dir=data_dir, split=type)
+    else:
+        raise NotImplementedError(f"Dataset {dataset_name} is not supported.")
+
+    return dataset
+
+task_to_keys = {
+    "cola": ("sentence", None),
+    "mnli": ("premise", "hypothesis"),
+    "mnli-mm": ("premise", "hypothesis"),
+    "mrpc": ("sentence1", "sentence2"),
+    "qnli": ("question", "sentence"),
+    "qqp": ("question1", "question2"),
+    "rte": ("sentence1", "sentence2"),
+    "sst2": ("sentence", None),
+    "stsb": ("sentence1", "sentence2"),
+    "wnli": ("sentence1", "sentence2"),
+    "lambada": ("text", None),
+    "wikitext": ("text", None),
+}
+
+default_params = {
+    "cola": {"num_train_epochs": 50, "max_seq_length": 128},
+    "mnli": {"num_train_epochs": 5, "max_seq_length": 128},
+    "mrpc": {"num_train_epochs": 20, "max_seq_length": 128},
+    "sst2": {"num_train_epochs": 10, "max_seq_length": 128},
+    "stsb": {"num_train_epochs": 100, "max_seq_length": 128},
+    "qqp": {"num_train_epochs": 5, "max_seq_length": 128},
+    "qnli": {"num_train_epochs": 10, "max_seq_length": 128},
+    "rte": {"num_train_epochs": 50, "max_seq_length": 128},
+    "imdb": {"num_train_epochs": 30, "max_seq_length": 512},
+}
---
+++ examples/NLG/src/utils/metric_utils.py
@@ -0,0 +1,80 @@
+from scipy.stats import pearsonr, spearmanr
+from sklearn.metrics import matthews_corrcoef, f1_score
+from torch.nn import CrossEntropyLoss
+import torch
+
+def simple_accuracy(preds, labels):
+    return (preds == labels).mean()
+
+
+def acc_and_f1(preds, labels):
+    acc = simple_accuracy(preds, labels)
+    f1 = f1_score(y_true=labels, y_pred=preds)
+    return {
+        "acc": acc,
+        "f1": f1,
+        "acc_and_f1": (acc + f1) / 2,
+    }
+
+
+def pearson_and_spearman(preds, labels):
+    pearson_corr = pearsonr(preds, labels)[0]
+    spearman_corr = spearmanr(preds, labels)[0]
+    return {
+        "pearson": pearson_corr,
+        "spearmanr": spearman_corr,
+        "corr": (pearson_corr + spearman_corr) / 2,
+    }
+
+def simple_ppl(preds, labels):
+    # NOTE: older version, it seems to calculate the wrong PPL
+    # loss = torch.sum(
+    #     torch.nn.functional.log_softmax(preds, dim=-1)
+    #     * torch.nn.functional.one_hot(labels, num_classes=preds.shape[-1]),
+    # )
+    # count = torch.sum(labels != tokenizer.pad_token_id)
+    # logging.info(f"loss: {loss}, count: {count}")
+    # perplexity = torch.exp(-loss / count)
+
+    # NOTE: reference: https://github.com/huggingface/transformers/issues/473
+    shift_logits = preds.contiguous()
+    shift_labels = labels.contiguous()
+    # Flatten the tokens
+    loss_fct = CrossEntropyLoss()
+    cls_loss = loss_fct(
+        shift_logits.view(-1, shift_logits.size(-1)),
+        shift_labels.view(-1),
+    )
+    perplexity = torch.exp(cls_loss)
+    return perplexity
+
+def compute_metrics(task_name, preds, labels):
+    assert len(preds) == len(labels)
+    if task_name == "cola":
+        return {"mcc": matthews_corrcoef(labels, preds)}
+    elif task_name == "sst2":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "mrpc":
+        return acc_and_f1(preds, labels)
+    elif task_name == "stsb":
+        return pearson_and_spearman(preds, labels)
+    elif task_name == "qqp":
+        return acc_and_f1(preds, labels)
+    elif task_name == "mnli":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "mnli-mm":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "qnli":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "rte":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "wnli":
+        return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "imdb":
+        return {"imdb": simple_accuracy(preds, labels)}
+    elif task_name == "wiki":
+        return {"ppl": simple_ppl(preds, labels)}
+    elif task_name == "lambada":
+        return {"ppl": simple_ppl(preds, labels)}
+    else:
+        raise KeyError(task_name)
